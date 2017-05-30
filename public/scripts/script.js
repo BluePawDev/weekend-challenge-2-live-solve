c47e10b26cd0067aae1c424bd45988fa5fe8e8bc
BluePawDev <jaruby@me.com>
2017-05-30 10:35:20 -0500

    Update

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -3,5 +3,37 @@ console.log('js/jq sourced');
 $(onReady);
 
 function onReady() {
+	$('.submit').on('click', function(event) {
+		event.preventDefault();
+		var firstNum = $('.inputOne').val();
+		var secondNum = $('.inputTwo').val();
+		var operator = $('.operator').val();
+		console.log(firstNum, secondNum, operator);
 
+
+		// capturing values in object
+
+
+		// sending data to server
+		calculate(firstNum, secondNum, operator);
+
+
+	});
+}
+
+function calculate(firstValue, secondValue, operation) {
+	var objToSend = {
+		x: firstValue,
+		y: secondValue,
+		type: operation
+	}
+
+	$.ajax({
+		type: 'POST',
+		url: '/calculate',
+		data: objToSend,
+		success: function(response) {
+			console.log(response);
+		}
+	})
 }
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
 
 
 
@@ -16,14 +17,32 @@ app.use(express.static('public'));
 // Base URL
 app.get('/', function(req, res) {
 	console.log('Base URL hit');
-	res.sendFile(path.resolve('views/index.html'));
+	res.sendFile(path.resolve('views/index.html'))
 });
+
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
 // Recieve math request
 app.post('/calculate', function(req, res) {
-	console.log();
+	console.log(req.body);
+	var valueForRes = calculations(req.body);
+
 });
 
-
+function calculations(objectToCalc) {
+	switch (objToCalc.type) {
+		case 'add':
+			return Number(objToCalc.x) + Number(objToCalc.y);
+		case 'subtract':
+			return Number(objToCalc.x) - Number(objToCalc.y);
+		case 'multiply':
+			return Number(objToCalc.x) * Number(objToCalc.y);
+		case 'divide':
+			return Number(objToCalc.x) / Number(objToCalc.y);
+		default:
+	}
+}
 
 // Send answer back
